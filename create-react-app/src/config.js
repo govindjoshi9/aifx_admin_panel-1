e382ff00f1ca64478683820b6c8659ff16720956
Govind Prasad <79787938+govindjoshi9@users.noreply.github.com>
2023-06-30 15:19:56 +0530

    Update config.js

--- create-react-app/src/config.js
+++ create-react-app/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   // basename: only at build time to set, and Don't add '/' at end off BASENAME for breadcrumbs, also Don't put only '/' use blank('') instead,
   // like '/berry-material-react/react/default'
-  basename: '',
+  basename: '/Uapp',
   defaultPath: '/dashboard/',
   fontFamily: `'Poppins', sans-serif`,
   borderRadius: 24
